9c2b5f0a1b57c4e34de665531716961d08a8b6af
ampuEus <ampueus@gmail.com>
2025-02-07 19:52:46 +0100

    An other try to fix MIME error

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://ampuEus.github.io',
+    site: 'https://ampueus.github.io',
     base: '/train-modeler',
 });
